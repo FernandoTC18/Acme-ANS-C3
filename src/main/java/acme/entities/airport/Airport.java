3d2df99594ded6f254151400f06d3aaf343bde12
josemgarciar <josgarros@alum.us.es>
2025-02-26 20:12:45 +0100

    Feat: Airport new entity Closes #43

---
+++ src/main/java/acme/entities/airport/Airport.java
@@ -0,0 +1,65 @@
+
+package acme.entities.airport;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidEmail;
+import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Airport extends AbstractEntity {
+
+	public static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(min = 0, max = 50)
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{3}$")
+	@Column(unique = true)
+	private String				iataCode;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private OperationalScope	operationalScope;
+
+	@Mandatory
+	@ValidString
+	@Automapped
+	private String				city;
+
+	@Mandatory
+	@ValidString(min = 0, max = 50)
+	@Automapped
+	private String				country;
+
+	@Optional
+	@ValidUrl
+	@Automapped
+	private String				webSite;
+
+	@Optional
+	@ValidEmail
+	@Automapped
+	private String				email;
+
+	@Optional
+	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	@Automapped
+	private String				phoneNumber;
+
+}
