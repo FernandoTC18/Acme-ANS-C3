772edf025c7e4a3d63bff6db8d4931415da3ac1c
Samuel Tamayo <samtambal@alum.us.es>
2025-02-28 03:48:25 +0100

    Feat: Review Entity

---
+++ src/main/java/acme/entities/review/Review.java
@@ -0,0 +1,63 @@
+
+package acme.entities.review;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Review {
+
+	// Serialisation version --------------------------------------------------
+
+	public static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(min = 0, max = 50)
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Automapped
+	private Date				moment;
+
+	@Mandatory
+	@ValidString(min = 0, max = 50)
+	@Automapped
+	private String				subject;
+
+	@Mandatory
+	@ValidString(min = 0, max = 50)
+	@Automapped
+	private String				text;
+
+	@Optional
+	@ValidNumber(min = 0, max = 10)
+	@Automapped
+	private Double				score;
+
+	@Optional
+	@Valid
+	@Automapped
+	private Boolean				recommended;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
