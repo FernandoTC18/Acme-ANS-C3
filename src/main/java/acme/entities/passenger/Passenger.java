ccc25c1d4fa9dec07be155f84c374e931fac8c58
josemgarciar <josgarros@alum.us.es>
2025-03-05 18:08:00 +0100

    Feat: Student #2 - D02 - IR5 - Create the entity Passenger (Booking entity to create)

---
+++ src/main/java/acme/entities/passenger/Passenger.java
@@ -0,0 +1,57 @@
+
+package acme.entities.passenger;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidEmail;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Passenger {
+
+	//Serialisation version --------------------------------------------------
+
+	public static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------	
+
+	@Mandatory
+	@ValidString(max = 256)
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@ValidEmail
+	@Automapped
+	private String				email;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z0-9]{6,9}$")
+	@Automapped
+	private String				passportNumber;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				birth;
+
+	@Optional
+	@ValidString(max = 51)
+	@Automapped
+	private String				specialNeeds;
+
+	// Relationships ----------------------------------------------------------
+
+}
