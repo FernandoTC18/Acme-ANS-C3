3d937cab6ff19f737f50085a48d18774e705a2c6
Petercrgz <petcargon@alum.us.es>
2025-03-06 10:41:31 +0100

    Corrections at entity Flight with a repository , refs #127

---
+++ src/main/java/acme/entities/flight/FlightRepository.java
@@ -0,0 +1,28 @@
+
+package acme.entities.flight;
+
+import java.util.Date;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+
+@Repository
+public interface FlightRepository extends AbstractRepository {
+
+	@Query("select min(l.scheduledDeparture) from Leg l where l.flight.id=:flightId")
+	Date computeScheduledDepartureByFlight(int flightId);
+
+	@Query("select max(l.scheduledArrival) from Leg l where l.flight.id=:flightId")
+	Date computeScheduledArrivalByFlight(int flightId);
+
+	@Query("select l.departureAirport.city from Leg l where l.flight.id=:flightId order by l.scheduledDeparture asc")
+	String computeOriginCityByFlight(int flightId);
+
+	@Query("select l.arrivalAirport.city from Leg l where l.flight.id=:flightId order by l.scheduledArrival dsc")
+	String computeArrivalCityByFlight(int flightId);
+
+	@Query("select count(l) from Leg l where l.flight.id=:flightId")
+	Integer computeLegsNumberByFlight(int flightId);
+}
