3923a853cb81d70b5602f48694707f82ee5c90be
Petercrgz <petcargon@alum.us.es>
2025-03-09 17:54:37 +0100

    Realm Manager created , refs #180

---
+++ src/main/java/acme/constraints/ManagerValidator.java
@@ -0,0 +1,51 @@
+
+package acme.constraints;
+
+import javax.validation.ConstraintValidatorContext;
+
+import acme.client.components.validation.AbstractValidator;
+import acme.client.components.validation.Validator;
+import acme.realms.Manager;
+
+@Validator
+public class ManagerValidator extends AbstractValidator<ValidManager, Manager> {
+
+	// Internal state ---------------------------------------------------------
+
+	// ConstraintValidator interface ------------------------------------------
+
+	@Override
+	protected void initialise(final ValidManager annotation) {
+		assert annotation != null;
+	}
+
+	@Override
+	public boolean isValid(final Manager manager, final ConstraintValidatorContext context) {
+		assert context != null;
+
+		boolean result;
+
+		if (manager == null)
+			super.state(context, false, "*", "javax.validation.constraints.NotNull.message");
+		else {
+			String name;
+			String nameIdentifier;
+			String initials;
+			boolean correctIdentifierNumber;
+
+			name = manager.getIdentity().getName();
+
+			nameIdentifier = manager.getIdentifierNumber().substring(0, manager.getIdentifierNumber().length() - 6);
+
+			initials = nameIdentifier.length() == 2 ? name.substring(0, 2) : name.substring(0, 3);
+
+			correctIdentifierNumber = initials == nameIdentifier;
+
+			super.state(context, correctIdentifierNumber, "identifiernumber", "acme.validation.manager.identifiernumber.message");
+
+		}
+		result = !super.hasErrors(context);
+
+		return result;
+	}
+}
