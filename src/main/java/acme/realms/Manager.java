26dd727514bec0f128162b260fe53f3822b289d0
Petercrgz <petcargon@alum.us.es>
2025-03-11 19:44:16 +0100

    fix Manager,closes #237

--- src/main/java/acme/realms/Manager.java
+++ src/main/java/acme/realms/Manager.java
@@ -32,7 +32,7 @@ public class Manager extends AbstractRole {
 
 	// Attributes -------------------------------------------------------------
 	@Mandatory
-	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$", min = 8, max = 9)
+	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$")
 	@Column(unique = true)
 	private String				identifierNumber;
 
