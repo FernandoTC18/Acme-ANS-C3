0de4b1dcf21de0d1a2ddcd4b5fa972ad1775f5ae
Julián Romero Parejo <114395879+julrompar@users.noreply.github.com>
2025-04-03 20:57:47 +0200

    Fixes issues on FlightAssignment logic. Closes #570

--- src/main/java/acme/features/flightCrew/flightAssignment/FlightCrewFlightAssignmentCreateService.java
+++ src/main/java/acme/features/flightCrew/flightAssignment/FlightCrewFlightAssignmentCreateService.java
@@ -36,42 +36,55 @@ public class FlightCrewFlightAssignmentCreateService extends AbstractGuiService<
 	@Override
 	public void load() {
 		FlightAssignment assignment;
+		int id;
+		FlightCrew member;
+		
+		id = super.getRequest().getPrincipal().getActiveRealm().getId();
+		member = this.repository.findCrewById(id);
+		
 
 		assignment = new FlightAssignment();
+		assignment.setDuty(null);
+		assignment.setLastUpdate(null);
+		assignment.setLeg(null);
+		assignment.setRemarks(null);
+		assignment.setStatus(null);
 		assignment.setDraftMode(true);
+		assignment.setFlightCrewMember(member);
 
 		super.getBuffer().addData(assignment);
 	}
 
 	@Override
 	public void bind(final FlightAssignment assignment) {
-		super.bindObject(assignment, "duty", "lastUpdate", "status", "remarks", "leg", "flightCrewMember");
+		super.bindObject(assignment, "duty", "lastUpdate", "status", "remarks", "leg");
 	}
 
 	@Override
 	public void validate(final FlightAssignment assignment) {
-
-		FlightCrew member;
-		Collection<Leg> legs;
-		Long pilotNumber;
-		Long copilotNumber;
-
-		member = super.getRequest().getData("flightCrewMember", FlightCrew.class);
-		super.state(member.getAvailability() == FlightCrewAvailability.AVAILABLE, "flightCrewMember", "acme.validation.unavailable-crew-member.message");
-
-		Date currentMoment = MomentHelper.getCurrentMoment();
-		Timestamp moment = Timestamp.from(currentMoment.toInstant());
-		legs = this.repository.getLegsByMemberId(moment, member.getId());
-		super.state(legs.isEmpty(), "leg", "acme.validation.assigned-leg.message");
-
-		pilotNumber = this.repository.countMembersByDuty(member.getId(), Optional.of(Duty.PILOT));
-		copilotNumber = this.repository.countMembersByDuty(member.getId(), Optional.of(Duty.COPILOT));
-
-		if (assignment.getDuty() == Duty.PILOT)
-			super.state(pilotNumber < 1, "flightCrewMember", "acme.validation.number-of-pilot.message");
-
-		if (assignment.getDuty() == Duty.COPILOT)
-			super.state(copilotNumber < 1, "flightCrewMember", "acme.validation.number-of-copilot.message");
+//
+//		FlightCrew member;
+//		Collection<Leg> legs;
+//		Long pilotNumber;
+//		Long copilotNumber;
+//
+//		member = super.getRequest().getData("flightCrewMember", FlightCrew.class);
+//			super.state(member.getAvailability() == FlightCrewAvailability.AVAILABLE, "flightCrewMember", "acme.validation.unavailable-crew-member.message");		
+//		
+//
+//		Date currentMoment = MomentHelper.getCurrentMoment();
+//		Timestamp moment = Timestamp.from(currentMoment.toInstant());
+//		legs = this.repository.getLegsByMemberId(moment, member.getId());
+//		super.state(legs.isEmpty(), "leg", "acme.validation.assigned-leg.message");
+//
+//		pilotNumber = this.repository.countMembersByDuty(member.getId(), Optional.of(Duty.PILOT));
+//		copilotNumber = this.repository.countMembersByDuty(member.getId(), Optional.of(Duty.COPILOT));
+//
+//		if (assignment.getDuty() == Duty.PILOT)
+//			super.state(pilotNumber < 1, "flightCrewMember", "acme.validation.number-of-pilot.message");
+//
+//		if (assignment.getDuty() == Duty.COPILOT)
+//			super.state(copilotNumber < 1, "flightCrewMember", "acme.validation.number-of-copilot.message");
 	}
 
 	@Override
@@ -98,7 +111,7 @@ public class FlightCrewFlightAssignmentCreateService extends AbstractGuiService<
 		dutyChoices = SelectChoices.from(Duty.class, assignment.getDuty());
 		memberChoices = SelectChoices.from(members, "employeeCode", assignment.getFlightCrewMember());
 
-		dataset = super.unbindObject(assignment, "duty", "lastUpdate", "status", "remarks", "leg", "flightCrewMember", "draftMode");
+		dataset = super.unbindObject(assignment, "duty", "lastUpdate", "status", "remarks", "leg", "draftMode");
 		dataset.put("leg", legChoices.getSelected().getKey());
 		dataset.put("legs", legChoices);
 		dataset.put("status", statusChoices);
