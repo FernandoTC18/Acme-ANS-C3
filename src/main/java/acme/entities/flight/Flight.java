afafd57e3c4765b40a85f299fa43a47988badfcb
Petercrgz <petcargon@alum.us.es>
2025-05-24 15:23:58 +0200

    indicies for flight and leg with some upgrades at leg ,refs #680

--- src/main/java/acme/entities/flight/Flight.java
+++ src/main/java/acme/entities/flight/Flight.java
@@ -4,7 +4,9 @@ package acme.entities.flight;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.validation.Valid;
 
@@ -24,6 +26,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(indexes = {
+	@Index(columnList = "draftMode"), @Index(columnList = "id")
+})
 @ValidFlight
 public class Flight extends AbstractEntity {
 
